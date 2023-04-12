3f9ac09a08e8a7c6f9eba9603ec22126e1da2187
mannan49 <mannannasir49@gmail.com>
2023-04-12 21:45:22 +0500

    Added basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/redux_cocktail">
         <App />
       </BrowserRouter>
     </Provider>
